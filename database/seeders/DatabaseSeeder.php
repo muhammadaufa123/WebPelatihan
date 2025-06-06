e230e867af9c2b867450fcb65c54cc01289fef33
muhammadaufa123 <116171585+muhammadaufa123@users.noreply.github.com>
2025-06-07 05:28:21 +0700

    Add course mode and level tables with seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RolePermissionSeeder::class);
+        $this->call([
+            RolePermissionSeeder::class,
+            CourseModeSeeder::class,
+            CourseLevelSeeder::class,
+        ]);
     }
 }
